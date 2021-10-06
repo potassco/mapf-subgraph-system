bb5bebf341a1381b76e84621b3105fe6075106c9
svancaj <jirka.svanc@gmail.com>
2021-10-06 20:44:56 +0200

    Update 0parse_res.sh

--- statistics/0parse_res.sh
+++ statistics/0parse_res.sh
@@ -9,7 +9,7 @@ do
 	else
 		max=$(wc -l $name*.res | head -n -1 | sort | tail -n1 | sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
 	fi
-	for file in $name\_b\_asp.res $name\_c\_asp.res $name\_m\_asp.res $name\_p\_asp.res $name\_b\_sat.res $name\_c\_sat.res $name\_m\_sat.res $name\_p\_sat.res 
+	for file in $name\_b\_asp.res $name\_c\_asp.res $name\_m\_asp.res $name\_p\_asp.res $name\_b\_asp-teg.res $name\_c\_asp-teg.res $name\_m\_asp-teg.res $name\_p\_asp-teg.res $name\_b\_sat.res $name\_c\_sat.res $name\_m\_sat.res $name\_p\_sat.res $name\_cbs.res
 	do
 		if [ -f "$file" ]
 		then
@@ -25,15 +25,26 @@ do
 			do
 				printf '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\n' >> $file
 			done
-		else
+		fi
+
+		if echo "$file" | grep sat
+		then
 			for i in $(seq $size)
 			do
 				printf '\t\t\t\t\t\t\t\n' >> $file
 			done
 		fi
+
+		if echo "$file" | grep cbs
+		then
+			for i in $(seq $size)
+			do
+				printf '\t\t\t\n' >> $file
+			done
+		fi
 	done
 
-	paste $name\_b\_asp.res $name\_c\_asp.res $name\_m\_asp.res $name\_p\_asp.res $name\_b\_sat.res $name\_c\_sat.res $name\_m\_sat.res $name\_p\_sat.res > $name.tmp
+	paste $name\_b\_asp.res $name\_c\_asp.res $name\_m\_asp.res $name\_p\_asp.res $name\_b\_asp-teg.res $name\_c\_asp-teg.res $name\_m\_asp-teg.res $name\_p\_asp-teg.res $name\_b\_sat.res $name\_c\_sat.res $name\_m\_sat.res $name\_p\_sat.res $name\_cbs.res > $name.tmp
 done
 
 cat *.tmp > 0results.txt
